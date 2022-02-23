0149e7d90ef58bcd0565d82252d7e4f6727a3780
Anish <96220428+Anishsuresh3@users.noreply.github.com>
2022-02-23 12:33:13 +0000

    ..

--- p2original.c
+++ p2original.c
@@ -26,8 +26,6 @@ void output(float x1, float y1, float x2, float y2,float x3, float y3, int istri
   else{
     printf("The given coordinates : (%f,%f),(%f,%f),(%f,%f) does not form a triangle\n",x1,y1,x2,y2,x3,y3);
   }
-  
-    
 }
 int main()
 {
